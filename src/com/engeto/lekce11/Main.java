e3e592e1eeebb5cb20c775865989024143bd4464
Daniel Pech <Pech.Daniel@seznam.cz>
2022-06-23 14:36:36 +0200

    2.nd commit MyBatis Added + configuration, Item class added and reader has been added. Everything works fine ... for now :)

--- src/com/engeto/lekce11/Main.java
+++ src/com/engeto/lekce11/Main.java
@@ -1,9 +1,18 @@
 package com.engeto.lekce11;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        Reader reader = Resources.getResourceAsReader("MyBatisConfig.xml");
+        SqlSessionFactory sqlFactory = new SqlSessionFactoryBuilder().build(reader);
 
 
     }
